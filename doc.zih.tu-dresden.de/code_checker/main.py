d08cd1038cec0716e982f5af00afe26c774282fb
Michael Müller <michael.mueller12@tu-dresden.de>
2021-07-15 10:42:20 +0200

    Skript can find all code blocks

--- doc.zih.tu-dresden.de/code_checker/main.py
+++ doc.zih.tu-dresden.de/code_checker/main.py
@@ -4,20 +4,30 @@
 import os
 import re
 
+
+def extract(content, f_name):
+  re_code = re.compile('```(.*?)```', re.DOTALL)
+  
+  matches = re_code.findall(content)
+  # TODO filter for testing keyword here!
+  code_snippets = ['\n'.join(x.split('\n')[1:]) for x in matches] #if 'Bash' in x.split('\n')[0]]
+  if code_snippets:
+    print('===='+f_name+'====')
+    print('\n------------\n'.join(code_snippets))
+    # TODO Do pipeline tests here!
+
+
 def main():
   exec_dir = os.path.dirname(__file__)
   DOC_PATH = os.path.join(exec_dir, "..", "docs")
-  TEST_FILE = os.path.join(DOC_PATH, 'jobs_and_resources', 'slurm.md')
-  with open(TEST_FILE, 'r') as f:
-    content = f.read()
-  re_code = re.compile('```(.*?)```', re.DOTALL)
-  results = re_code.findall(content)
-  # This contains only code
-  # TODO replace "Bash" in if with the testing keyword.
-  # TODO loop over all files
-  # TODO move to function!
-  code_snippets = [x.split('\n')[1:] for x in results if 'Bash' in x.split('\n')[0]]
+
+  # Loop .md Files
+  for subdir, dirs, files in os.walk(DOC_PATH):
+    for f_name in files:
+      if os.path.splitext(f_name)[-1] == '.md':
+        with open(os.path.join(subdir, f_name), 'r') as f:
+          extract(f.read(), f_name)
+
 
 if __name__ == "__main__":
-  main()
-  
+  main()  
