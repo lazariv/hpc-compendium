55798707913236b6ca6b2aaa3c838d69dc0e6956
Jan Frenzel <jan.frenzel@tu-dresden.de>
2021-10-19 10:24:46 +0200

    Revert parts of previous commit.

--- doc.zih.tu-dresden.de/util/grep-forbidden-words.sh
+++ doc.zih.tu-dresden.de/util/grep-forbidden-words.sh
@@ -18,8 +18,8 @@ i	file \+system	HDFS
 i	\<taurus\>	taurus\.hrsk	/taurus	/TAURUS
 i	\<hrskii\>
 i	hpc[ -]\+da\>
-i	ATTACHURL
-i	\<todo\>
+i	attachurl
+i	\<todo\>	<!--.*todo.*-->
 i	[[:space:]]$
 i	\(alpha\|ml\|haswell\|romeo\|gpu\|smp\|julia\|hpdlf\|scs5\)-\?\(interactive\)\?[^a-z]*partition
 i	\[\s\?\(documentation\|here\|this \(link\|page\|subsection\)\|slides\?\|manpage\)\s\?\]
@@ -113,7 +113,7 @@ for f in $files; do
             grepflag=-i
           ;;
         esac
-        if egrep -n $grepflag $color "$pattern" "$f" | grepExceptions "${exceptionPatternsArray[@]}" ; then
+        if grep -n $grepflag $color "$pattern" "$f" | grepExceptions "${exceptionPatternsArray[@]}" ; then
           ((cnt=cnt+1))
         fi
       done <<< $exceptionPatterns
