5482ee5843272ea6984a0d880c566e123ac026d1
Jan Frenzel <jan.frenzel@tu-dresden.de>
2021-11-12 10:27:20 +0100

    Corrected wrong script name in test-grep-forbidden-patterns.sh.

--- doc.zih.tu-dresden.de/util/test-grep-forbidden-patterns.sh
+++ doc.zih.tu-dresden.de/util/test-grep-forbidden-patterns.sh
@@ -2,7 +2,7 @@
 
 expected_match_count=32
 
-number_of_matches=$(bash ./doc.zih.tu-dresden.de/util/grep-forbidden-words.sh -f doc.zih.tu-dresden.de/util/grep-forbidden-patterns.testdoc -c -c | grep "Forbidden Patterns:" | sed -e 's/.*: //' | sed -e 's/ matches.*//')
+number_of_matches=$(bash ./doc.zih.tu-dresden.de/util/grep-forbidden-patterns.sh -f doc.zih.tu-dresden.de/util/grep-forbidden-patterns.testdoc -c -c | grep "Forbidden Patterns:" | sed -e 's/.*: //' | sed -e 's/ matches.*//')
 
 if [ $number_of_matches -eq $expected_match_count ]; then
 	echo "Test OK"
