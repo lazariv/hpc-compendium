ca8be42bac70626c73e83309c0fed1a6c1d57849
Jan Frenzel <jan.frenzel@tu-dresden.de>
2021-11-25 17:15:50 +0100

    Added basedir variable to check-no-floating.sh.

--- doc.zih.tu-dresden.de/util/check-no-floating.sh
+++ doc.zih.tu-dresden.de/util/check-no-floating.sh
@@ -4,11 +4,12 @@ if [ ${#} -ne 1 ]; then
   echo "Usage: ${0} <path>"
 fi
 
-DOCUMENT_ROOT=${1}
+basedir=${1}
+DOCUMENT_ROOT=${basedir}/docs
 maxDepth=4
-expectedFooter="$DOCUMENT_ROOT/docs/legal_notice.md $DOCUMENT_ROOT/docs/accessibility.md $DOCUMENT_ROOT/docs/data_protection_declaration.md"
+expectedFooter="$DOCUMENT_ROOT/legal_notice.md $DOCUMENT_ROOT/accessibility.md $DOCUMENT_ROOT/data_protection_declaration.md"
 
-MSG=$(find ${DOCUMENT_ROOT}/docs -name "*.md" | awk -F'/' '{print $0,NF}' | while IFS=' ' read string depth
+MSG=$(find ${DOCUMENT_ROOT} -name "*.md" | awk -F'/' '{print $0,NF}' | while IFS=' ' read string depth
   do
     #echo "string=${string} depth=${depth}"
 
@@ -17,14 +18,14 @@ MSG=$(find ${DOCUMENT_ROOT}/docs -name "*.md" | awk -F'/' '{print $0,NF}' | whil
       echo "max depth ($maxDepth) exceeded for ${string}"
     fi
 
-    md=${string#${DOCUMENT_ROOT}/docs/}
+    md=${string#${DOCUMENT_ROOT}/}
 
     # md included in nav 
-    numberOfReferences=`sed -n '/nav:/,/^$/p' ${DOCUMENT_ROOT}/mkdocs.yml | grep -c ${md}`
+    numberOfReferences=`sed -n '/nav:/,/^$/p' ${basedir}/mkdocs.yml | grep -c ${md}`
     if [ $numberOfReferences -eq 0 ]; then
       # fallback: md included in footer 
       if [[ "${expectedFooter}" =~ ${string} ]]; then
-        numberOfReferencesInFooter=`sed -n '/footer:/,/^$/p' ${DOCUMENT_ROOT}/mkdocs.yml | grep -c /${md%.md}`
+        numberOfReferencesInFooter=`sed -n '/footer:/,/^$/p' ${basedir}/mkdocs.yml | grep -c /${md%.md}`
         if [ $numberOfReferencesInFooter -eq 0 ]; then
           echo "${md} is not included in footer"
         elif [ $numberOfReferencesInFooter -ne 1 ]; then
